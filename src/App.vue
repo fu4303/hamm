0fff3efdfb6ab31373522f539740c41d71006ec4
Alexander Garcia <alexander.schmidt.garcia@gmail.com>
2018-09-03 11:08:53 -0400

    features/subnavbar add components

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="app">
     <vue-nav />
+    <vue-sub-nav />
     <router-view/>
   </div>
 </template>
 
 <script>
 import VueNav from './components/VueNav';
+import VueSubNav from './components/VueSubNav';
 export default {
   components: {
-    VueNav
+    VueNav,
+    VueSubNav
   }
 }
 </script>
